2436a4b28f5609259b9eb4f6a5a4372720b48e7a
andreynnt <andrejbabkov@gmail.com>
2018-04-25 19:34:30 +0300

    lil fix linter errors

--- src/main/java/project/services/UserService.java
+++ src/main/java/project/services/UserService.java
@@ -3,7 +3,6 @@ package project.services;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import project.models.UserModel;
 import project.rowmapper.ApiRowMapper;
 import java.util.ArrayList;
